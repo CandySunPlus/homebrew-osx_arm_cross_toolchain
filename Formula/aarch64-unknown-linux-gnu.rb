b2bd095e92d7dd11679d03ec7aa833d8ef85e6ea
Fengming Sun <niksun@FengmingdeMacBook-Pro.local>
2021-06-04 21:25:34 +0800

    fix download url

--- Formula/aarch64-unknown-linux-gnu.rb
+++ Formula/aarch64-unknown-linux-gnu.rb
@@ -1,7 +1,7 @@
 class Aarch64UnknownLinuxGnu < Formula
   desc "Pre-built ARM/Linux C cross-compilers for MacOS"
   homepage "https://github.com/thinkski/osx-arm-linux-toolchains"
-  url "https://github.com/CandySunPlus/osx_arm_cross_toolchain/releases/download/8.3.0/aarch64-unknown-linux-gnu.tar.xz"
+  url "https://github.com/CandySunPlus/homebrew-osx_arm_cross_toolchain/releases/download/8.3.0/aarch64-unknown-linux-gnu.tar.xz"
   version "8.3.0"
   sha256 "24318a26556b67b23d2527596c0118b997237d771e0cb5ea9eae7d4b8d784a39"
 
