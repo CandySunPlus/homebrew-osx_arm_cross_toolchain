029b1e798fd96723f92b6af7c4332a3c044604bd
Fengming Sun <niksun@FengmingdeMacBook-Pro.local>
2021-06-08 14:15:17 +0800

    update toolchain

--- Formula/aarch64-unknown-linux-gnu.rb
+++ Formula/aarch64-unknown-linux-gnu.rb
@@ -3,7 +3,7 @@ class Aarch64UnknownLinuxGnu < Formula
   homepage "https://crosstool-ng.github.io/"
   url "https://github.com/CandySunPlus/homebrew-osx_arm_cross_toolchain/releases/download/11.1.0/aarch64-unknown-linux-gnu.tar.xz"
   version "11.1.0"
-  sha256 "3075fa5c4a7e5fe4ea39830cc71c3e65a8ccc957f0aeb61e0425d658e1028ba9"
+  sha256 "19a1d51b887211fbd985f72b4c0fd7962f91ba398deeb8e689bd0ce009b151bc"
 
   def install
     (prefix/"toolchain").install Dir["./*"]
